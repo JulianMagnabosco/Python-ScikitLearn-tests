2d4406c1699fc0673d9941f0d7d399af7693c7a7
Raishin0 <julian.magnabosco@gmail.com>
2025-05-10 02:27:13 -0300

    avances

---
+++ forest-regression.py
@@ -0,0 +1,42 @@
+import numpy as np
+from sklearn import datasets, metrics,model_selection,ensemble
+import matplotlib.pyplot as plt
+
+boston = datasets.load_diabetes()
+# print("Keys")
+# print(boston.keys())
+# print()
+# print("DESCR")
+# print(boston.DESCR)
+# print()
+# print("Data Shape")
+# print(boston.data.shape)
+# print()
+# print("Data Shape")
+# print(boston.feature_names)
+
+x=boston.data[:,np.newaxis,2]
+# print(x)
+y=boston.target
+# print(y)
+
+x_train,x_test,y_train,y_test = model_selection.train_test_split(x,y,test_size=0.2)
+
+lr = ensemble.RandomForestRegressor(n_estimators=300,max_depth=8)
+lr.fit(x_train,y_train)
+
+y_predict = lr.predict(x_test)
+
+print("----Presicion----")
+print(lr.score(x_train,y_train))
+
+x_flat_test = np.ravel(x_test)
+x_grid=np.arange(min(x_flat_test),max(x_flat_test),0.1)
+x_grid=x_grid.reshape((len(x_grid),1))
+plt.scatter(x_test,y_test)
+plt.plot(x_grid,lr.predict(x_grid),color="red",linewidth=3)
+plt.xlabel("Datos x")
+plt.ylabel("Datos y")
+plt.title("Resultado")
+plt.show()
+
