c0741461f0a65b111d79c783b8af8bd6a3ebfe81
Raishin0 <julian.magnabosco@gmail.com>
2025-05-11 13:48:34 -0300

    avances

---
+++ forest-classification.py
@@ -0,0 +1,50 @@
+
+import numpy as np
+from sklearn import datasets, tree,model_selection,metrics,ensemble
+import matplotlib.pyplot as plt
+import pandas as pd  # doctest: +SKIP
+import numpy as np
+
+boston = datasets.load_breast_cancer()
+# print("Keys")
+# print(boston.keys())
+# print()
+# print("DESCR")
+# print(boston.DESCR)
+# print()
+# print("Data Shape")
+# print(boston.data.shape)
+# print()
+# print("Data Shape")
+# print(boston.feature_names)
+
+x=boston.data[:,np.newaxis,3]
+# print(x)
+y=boston.target
+# print(y)
+
+x_train,x_test,y_train,y_test = model_selection.train_test_split(x,y,test_size=0.2)
+
+lr = ensemble.RandomForestClassifier(criterion='entropy',n_estimators=10)
+lr.fit(x_train,y_train)
+
+y_predict = lr.predict(x_test)
+
+print("----Presicion----")
+print(metrics.accuracy_score(y_test,y_predict))
+print("----Exactitud----")
+print(metrics.recall_score(y_test,y_predict))
+print("----F1----")
+print(metrics.f1_score(y_test,y_predict))
+print("----ROC-AUC----")
+print(metrics.roc_auc_score(y_test,y_predict))
+print("----Matriz de confusion----")
+print(metrics.confusion_matrix(y_test,y_predict))
+
+# plt.scatter(x_test,y_test)
+# plt.plot(x_test,y_predict,color="red",linewidth=3)
+# plt.xlabel("Datos x")
+# plt.ylabel("Datos y")
+# plt.title("Resultado")
+# plt.show()
+
